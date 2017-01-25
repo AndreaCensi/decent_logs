8b70c952a777f4927cc4618bf706a4428e580c7e
Andrea Censi <censi@mit.edu>
2017-01-25 23:14:26 +0100

    no zip safe

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from setuptools import setup, find_packages
 
 version = "1.0"
 
-description = """ Simple library to have objects keeping their log messages. """ 
+description = """ Simple library to have objects keeping their log messages. """
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
+
 long_description = read('README.md')
-    
+
 
 setup(name='DecentLogs',
       author="Andrea Censi",
       author_email="andrea@cds.caltech.edu",
       url='http://github.com/AndreaCensi/decent_logs',
-      
+
       description=description,
       long_description=long_description,
       keywords="",
       license="",
-      
+
       classifiers=[
         'Development Status :: 4 - Beta',
         # 'Intended Audience :: Developers',
@@ -32,7 +32,7 @@ setup(name='DecentLogs',
 
       version=version,
       download_url='http://github.com/AndreaCensi/decent_logs/tarball/%s' % version,
-      
+
       entry_points={
         'console_scripts': [
        # 'comptests = comptests:main_comptests'
@@ -42,5 +42,5 @@ setup(name='DecentLogs',
       packages=find_packages('src'),
       install_requires=['PyContracts'],
       tests_require=['nose'],
+      zip_safe=False,
 )
-
